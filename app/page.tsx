02c1d4a5b49099103e4cf19dbddd84bfbcf81ab6
66Gramms <meszarosricsi01@gmail.com>
2026-02-15 14:28:04 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="min-h-screen w-full max-w-3xlpy-32 px-16 bg-white dark:bg-black sm:items-start mt-20">
+        <h1 className="text-6xl text-center w-full">Gallows</h1>
+        <h2 className="text-2xl text-center w-full">
+          Choose a difficulty to start playing
+        </h2>
+      </main>
+    </div>
+  );
+}
